5b0adcf997c9a7a1ab03c432935b437e8d7c7b7c
Fatima Amin <fatima.amin4official@gmail.com>
2020-08-20 13:57:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 // Write your code here!
 let elem = document.getElementById('main') ;
 elem.parentNode.removeChild(elem);
+let newHeader = document.createElement('h1');
+newHeader.id='victory';
+document.body.appendChild(newHeader)
