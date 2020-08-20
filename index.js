e481fc20c9391f505778eebfaa4cdb78cf421331
Fatima Amin <fatima.amin4official@gmail.com>
2020-08-20 13:58:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ let elem = document.getElementById('main') ;
 elem.parentNode.removeChild(elem);
 let newHeader = document.createElement('h1');
 newHeader.id='victory';
-document.body.appendChild(newHeader)
+newHeader.textContent ='YOUR-NAME is the champion';
+document.body.appendChild(newHeader);
